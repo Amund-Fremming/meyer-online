8dabb4dd5db8ba727b09fae15f3202634345fc43
amund-fremming <amund.fremming@gmail.com>
2023-08-23 10:54:52 +0200

    Small changes to Developer component

--- src/views/Developer.jsx
+++ src/views/Developer.jsx
@@ -1,27 +1,40 @@
 import React, { useEffect, useState } from 'react';
 import { handleLeaveGame } from '../util/databaseFunctions';
+import mePixelImage from "../img/me-pixel.png";
+import mePixelImageWink from "../img/me-pixel-wink.png";
+import mePixelImageSunglasses from "../img/me-pixel-sunglasses.png";
 
 /**
  * Short page about the developer and how to contact
  */
 const Developer = ({ username, documentRef, resetGameState }) => {
 
-  const [profile, setProfile] = useState("me-pixel.JPG");
+  const [profile, setProfile] = useState(mePixelImage);
+  const [timer, setTimer] = useState();
+  const [message, setMessage] = useState("");
 
   /**
    * Timer for the image to make a wink effect.
    */
+  useEffect(() => { setTimeout(() => setMessage("Click me!"), 6000) });
+
   useEffect(() => {
     const timer = setInterval(() => {
-      if(profile === "me-pixel.JPG") {
-        setProfile("me-pixel-wink.JPG");
+      if(profile === mePixelImage) {
+        setProfile(mePixelImageWink);
       } else {
-        setProfile("me-pixel.JPG");
+        setProfile(mePixelImage);
       }
     }, 1500);
+    setTimer(timer);
 
     return () => clearInterval(timer);
-  });
+  }, [profile]);
+
+  const handleClick = () => {
+    clearInterval(timer);
+    setProfile(mePixelImageSunglasses);
+  };
 
   return (
     <div className='flex flex-col justify-center items-center h-screen w-full'>
@@ -37,14 +50,16 @@ const Developer = ({ username, documentRef, resetGameState }) => {
 
       <p className='font-bradford text-[18px] font-serif'>Amund Fremming</p>
       <p className='text-gray-400 font-serif'>Engineer & CEO</p>
-      <img 
+      <img
+        onClick={handleClick} 
         className='w-[40%]'
-        src={require(`../img/${profile}`)}
+        src={profile}
       />
+      <p className='font-serif mt-1'>{message}</p>
       
       <div className='flex justify-between mt-20 w-[100px]'>
-        <p className='underline underline-offset-2 text-[18px] font-serif'><a href='https://twitter.com/amundfremming'>X</a></p>
-        <p className='underline underline-offset-2 text-[18px] font-serif'><a href="mailto:amund.fremming@gmail.com">Mail</a></p>
+        <p className='underline underline-offset-2 text-[16px] font-serif'><a href='https://fremminglabs-portfolio.web.app/'>MySite</a></p>
+        <p className='underline underline-offset-2 text-[16px] font-serif'><a href="mailto:amund.fremming@gmail.com">Mail</a></p>
       </div>
 
       <p className='flex relative bottom-[-220px] text-[16px] font-serif'> © 2023 FremmingLabs, Inc.</p>
