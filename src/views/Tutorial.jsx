9469ce97376ab5902d7c5850cba1ba39124e7995
amund-fremming <amund.fremming@gmail.com>
2023-09-04 10:13:38 +0200

    prod

--- src/views/Tutorial.jsx
+++ src/views/Tutorial.jsx
@@ -39,11 +39,11 @@ const Tutorial = ({ resetGameState }) => {
 
             <h1 className="font-serif mt-8 underline underline-offset-2 text-xl mb-2">Dice Hierarki</h1>
             <ol className="font-serif text-xs p-1 w-[300px] list-decimal">
-                <li className="py-1">The starting player rolls the dice secretly, keeping the result hidden from other players</li>
-                <li className="py-1">To pass the round the player needs to score higher than the player before</li>
-                <li className="py-1">The player then anonounces what dices he got without showing his dices, the player can also decide to lie if he gets a low dice number</li>
-                <li className="py-1">The turn passes to the next player clockwise</li>
-                <li className="py-1">When its your turn you have the choice to BUST, this means that you dont believe the last players dice scores, and think they are lying. If the player before lied you win, if not you loose.</li>
+                <li className="py-1">Meyer: 21</li>
+                <li className="py-1">Small Meyer: 31</li>
+                <li className="py-1">Pairs from 6 to 1</li>
+                <li className="py-1">Values from 65 to 32</li>
+                <li className="py-1">If someone scores 32, everybody drinks!</li>
             </ol>
 
             <div className="flex flex-col items-center justify-center mx-6 my-4">
@@ -59,24 +59,10 @@ const Tutorial = ({ resetGameState }) => {
 export default Tutorial;
 
 /*
-    GAMEPLAY
-    1. The starting player rolls the dice secretly, keeping the result hidden from other players
-    2. To pass the round the player needs to score higher than the player before
-    3. The player then anonounces what dices he got without showing his dices, the player can also decide to lie if he gets a low dice number 
-    4. The turn passes to the next player clockwise
-    5. When its your turn you have the choice to BUST, this means that you dont believe the last players dice scores, and think they are lying. If the player before lied we win, if not we loose.
-
-    Dice Score Hierarki
+   
     1. Meyer: 21
     2. Small Meyer: 31
     3. Pairs going from 6 to 1
     4. scores going down from 65 to 32
-
-    if a player gets 32, Everybody drinks!
-
-    GAME END
-    The game ends when players dont want to drink anymore
-
-    TUTORIAL WITH IMAGES OR VIDEO???
-
+    5. if a player gets 32, Everybody drinks!
  */
